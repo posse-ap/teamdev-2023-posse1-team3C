11bf2def135bd7c35536022385265232f766b35e
kazuki1023 <k.keio12569@gmail.com>
2023-04-19 22:05:06 +0900

    companyテーブル作成

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -62,5 +62,26 @@ CREATE TABLE ClientUsers (
   mail VARCHAR(255),
   password VARCHAR(255),
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "発行日時"
-);
-insert into ClientUsers (mail, password, created_at) values ("client@example.com", "$2y$10$csAFREneXMq1sdnuvOrFWe.ZW0kDM3Qigy1S0bhFJ3hhc6fgpMEIy", "2023-04-19 18:00:00");
+) CHARSET=utf8;
+insert into ClientUsers (mail, password) values ("client@example.com", "$2y$10$csAFREneXMq1sdnuvOrFWe.ZW0kDM3Qigy1S0bhFJ3hhc6fgpMEIy");
+
+DROP TABLE IF EXISTS Companies;
+CREATE TABLE Companies (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  company VARCHAR(255) COMMENT "企業名",
+  service VARCHAR(255) COMMENT "サービス名",
+  URL VARCHAR(255) COMMENT "サービスのURL",
+  mail VARCHAR(255) COMMENT "メールアドレス",
+  postcode VARCHAR(255) COMMENT "郵便番号",
+  address VARCHAR(255) COMMENT "住所",
+  phoneNumber VARCHAR(255) COMMENT "電話番号",
+  contactType VARCHAR(255) COMMENT "学生接触形態",
+  Date VARCHAR(255) COMMENT "対応可能日時",
+  started_at DATETIME COMMENT "開始日",
+  finished_at DATETIME COMMENT "終了日",
+  registered_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "登録日",
+  online VARCHAR(255) COMMENT "オンライン対応可"
+) CHARSET=utf8;
+
+insert into Companies (company, service, URL, mail, postcode, address, phoneNumber, contactType, Date, started_at, finished_at, online) values ("Test", "Testservice", "keio.jp", "li12569@keio.jp", "108-8345", "東京都港区三田２丁目１５−４５", "03-5427-1517", "オンライン", "平日 9-18時 土9-15時 日定休", "2023-05-01", "2023-06-30", "終日対応可");
+
