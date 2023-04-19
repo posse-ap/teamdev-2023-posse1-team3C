5bb29646f0894db6b6795a524810e656537d453a
kazuki1023 <k.keio12569@gmail.com>
2023-04-19 16:54:15 +0900

    ファイル構成変更

---
+++ src/client/components/link.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="">
+<link rel="stylesheet" href="">
+<link rel="stylesheet" href="../assets/styles/normalize.css">
+
+<!-- fontawesome読み込み -->
+<script src="https://kit.fontawesome.com/b4c9445cdc.js" crossorigin="anonymous" defer></script>
+<!-- jquery読み込み -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous" defer></script>
+<!-- apexchart読み込み -->
+<script src="https://unpkg.com/apexcharts/dist/apexcharts.min.js" defer></script>
