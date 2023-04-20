b989196f40c529c65a87663a4900502c738f273f
kazuki1023 <k.keio12569@gmail.com>
2023-04-20 10:55:14 +0900

    admin header見た目作成

--- src/admin/components/link.php
+++ src/admin/components/link.php
@@ -1,10 +1,8 @@
+
 <link rel="stylesheet" href="../assets/styles/normalize.css">
 <link rel="stylesheet" href="../../dist/output.css">
-<link rel="stylesheet" href="">
-<link rel="stylesheet" href="">
+<link rel="stylesheet" href="./assets/styles/top.css">
 
-<!-- swiper読み込み、スライダー作成用 -->
-<link　rel="stylesheet"　href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
 <!-- swiper読み込み、スライダー作成用 -->
 <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
 <!-- fontawesome読み込み -->
@@ -13,3 +11,6 @@
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous" defer></script>
 <!-- apexchart読み込み -->
 <script src="https://unpkg.com/apexcharts/dist/apexcharts.min.js" defer></script>
+<!-- swiper読み込み、スライダー作成用 -->
+<link　rel="stylesheet"　href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
+
