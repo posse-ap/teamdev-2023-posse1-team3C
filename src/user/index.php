71dc08aa15eb78916c430fa09419e3437e11a61d
dukken06 <kenxavi.8@gmail.com>
2023-04-20 18:14:18 +0900

    html要素の追加(項目のみ)

--- src/user/index.php
+++ src/user/index.php
@@ -11,9 +11,45 @@
     <h1>
       CRAFT
     </h1>
-    <div class="boozer-logo">
-      <img src="" alt="">
+    <div class="header-boozer-logo">
+      <img src="../../../img/boozer_logo_white.png" alt="boozer">
     </div>
   </header>
+  <main>
+    <div class="toppage-hero">
+
+    </div>
+    <div class="step-explanation-container">
+
+    </div>
+    <div class="user-voice-container">
+
+    </div>
+    <div class="common-question-container">
+
+    </div>
+  </main>
+  <footer>
+    <div class="footer-boozer-logo">
+      <img src="../../img/boozer-logo-white.png" alt="boozer">
+    </div>
+    <ul class="footer-link-list">
+      <li>
+        お問い合わせ
+      </li>
+      <li>
+        運営会社
+      </li>
+      <li>
+        利用規約
+      </li>
+      <li>
+        プライバシーポリシー
+      </li>
+    </ul>
+    <div class="footer-craft">
+      © 2023 CRAFT
+    </div>
+  </footer>
 </body>
 </html>
