6bb37e47e159995863b87a9f69a838cbee8786ae
dukken06 <kenxavi.8@gmail.com>
2023-04-20 17:29:42 +0900

    boozer_logo_white.pngをimgに追加

---
+++ src/user/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CRAFT Toppage</title>
+</head>
+<body>
+  <header>
+    <h1>
+      CRAFT
+    </h1>
+    <div class="boozer-logo">
+      <img src="" alt="">
+    </div>
+  </header>
+</body>
+</html>
