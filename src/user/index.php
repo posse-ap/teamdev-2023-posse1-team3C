7571b057c05ebe7f564557c518c90799709fcb40
dukken06 <kenxavi.8@gmail.com>
2023-04-20 23:45:50 +0900

    user画面トップページののhero,エージェントを探してみるボタン作成

--- src/user/index.php
+++ src/user/index.php
@@ -7,8 +7,10 @@
   <title>CRAFT Toppage</title>
   <link rel="stylesheet" href="./assets/styles/normalize.css">
   <link rel="stylesheet" href="./assets/styles/top.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
+  <!-- ヘッダー -->
   <header>
     <h1 class="header-title">
       CRAFT
@@ -18,11 +20,44 @@
     </div>
   </header>
   <main>
+    <!-- ヒーロー -->
     <div class="toppage-hero">
-
+      <div class="toppage-hero-comment">
+        <h2>
+        あなたにあった就活エージェント
+        <br>
+        選びを。まずは企業と話してみよう。
+        </h2>
+      </div>
     </div>
+    <!-- エージェントを探してみる -->
+    <div class="btn-search-for-agent">
+      <a href="./clientList.php">
+        <button>
+          エージェントを探してみる
+          <i class="fa-solid fa-caret-right"></i>
+        </button>
+      </a>
+    </div>
+    <!-- 申し込みするための３ステップ -->
     <div class="step-explanation-contents">
+      <h3>
+      \ 申し込みするための３ステップ /
+      </h3>
+      <p>
+        
+      </p>
+      <div class="step-container">
+        <div class="step-1">
 
+        </div>
+        <div class="step-2">
+
+        </div>
+        <div class="step-3">
+
+        </div>
+      </div>
     </div>
     <div class="user-voice-contents">
 
