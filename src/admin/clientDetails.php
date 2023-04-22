a76522be504cd69bfb74b4567fd1fb188b7aadb9
kazuki1023 <k.keio12569@gmail.com>
2023-04-22 19:22:01 +0900

    企業詳細の見た目作成

--- src/admin/clientDetails.php
+++ src/admin/clientDetails.php
@@ -4,13 +4,90 @@ include(dirname(__FILE__) . '/components/head.php');
 ?>
 
 </head>
+
 <body class="bg-gray-300">
   <!-- header読み込み -->
   <?php include(dirname(__FILE__) . '/components/header.php'); ?>
   <div class="wrapper">
     <!-- sidebar読み込み -->
     <?php include(dirname(__FILE__) . '/components/sidebar.php'); ?>
-    <main>
+    <main class="pt-20">
+      <section>
+        <h2>企業詳細情報</h2>
+      </section>
+      <section class="flex">
+        <h3>掲載状況</h3>
+        <span class="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">掲載中</span>
+        <span class="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">掲載停止中</span>
+        <button>変更</button>
+      </section>
+      <section>
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg mb-5 text-center">
+          <h3 class="text-center">企業詳細情報</h3>
+          <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <tbody>
+                <tr class="border-b border-gray-200 dark:border-gray-700">
+                  <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">企業名
+                  </th>
+                  <td class="px-6 py-4">
+                  </td>
+                </tr>
+                <tr class="border-b border-gray-200 dark:border-gray-700">
+                  <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">エージェントサービス名
+                  </th>
+                  <td class="px-6 py-4">
+                  </td>
+                </tr>
+                <tr class="border-b border-gray-200 dark:border-gray-700">
+                  <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">所在地
+                  </th>
+                  <td class="px-6 py-4">
+                  </td>
+                </tr>
+                <tr class="border-b border-gray-200 dark:border-gray-700">
+                  <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">連絡用電話番号
+                  </th>
+                  <td class="px-6 py-4">
+                  </td>
+                </tr>
+                <tr class="border-b border-gray-200 dark:border-gray-700">
+                  <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">連絡用メールアドレス
+                  </th>
+                  <td class="px-6 py-4">
+                  </td>
+                </tr>
+                <tr class="border-b border-gray-200 dark:border-gray-700">
+                  <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">営業時間
+                  </th>
+                  <td class="px-6 py-4">
+                  </td>
+                </tr>
+                <tr class="border-b border-gray-200 dark:border-gray-700">
+                  <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">公式サイトURL
+                  </th>
+                  <td class="px-6 py-4">
+                  </td>
+                </tr>
+                <tr class="border-b border-gray-200 dark:border-gray-700">
+                  <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">登録学生とのコンタクト形態
+                  </th>
+                  <td class="px-6 py-4">
+                  </td>
+                </tr>
+                <tr class="border-b border-gray-200 dark:border-gray-700">
+                  <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">オンライン対応の可否
+                  </th>
+                  <td class="px-6 py-4">
+                  </td>
+                </tr>
+            </tbody>
+          </table>
+        </div>
+      </section>
+      <section></section>
+      <section></section>
+      <section></section>
+      <button></button>
 
     </main>
   </div>
