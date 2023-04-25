4dcaa3cb2caa04467fd45ec3690fcfd190e6b94e
kazuki1023 <k.keio12569@gmail.com>
2023-04-25 13:10:04 +0900

    企業毎の学生情報を取得、人数取得

--- src/admin/clientDetails.php
+++ src/admin/clientDetails.php
@@ -123,10 +123,69 @@ include(dirname(__FILE__) . '/components/head.php');
           <button type="submit" name="resetSubmit" id="resetButton" class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">生成・再設定</button>
         </form>
       </section>
-      <!-- 登録学生情報 -->
-      <section></section>
       <!-- 学生情報テーブル -->
-      <section></section>
+      <section>
+        <div></div>
+        <div></div>
+        <div>
+          <div>総学生数：
+          <span><?php print_r($resultCountStudents["totalStudents"])?>人</span>
+          </div>
+          <div>(内無効学生数:
+            <span><?php print_r($resultCountStudentsInvalid["totalStudentsInvalid"])?>人)</span>
+          </div>
+        </div>
+      </section>
+      <!-- 登録学生情報 -->
+      <section>
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg mb-5 text-center">
+          <h3 class="text-center">企業基本情報</h3>
+          <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase dark:text-gray-400">
+              <tr>
+                <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                  登録学生氏名
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  学生ID番号
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  登録日時
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Status
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  詳細へ
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php foreach($CompaniesStudentsLink as $list) {?>
+              <tr class="border-b border-gray-200 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                  <?php echo $list["Name"] ?>
+                </th>
+                <td class="px-6 py-4">
+                  <?php echo $list["id"] ?>
+                </td>
+                <td class="px-6 py-4">
+                  <?php echo $list["registered_at"] ?>
+                </td>
+                <td class="px-6 py-4">
+                  <?php echo $list["status"] ?>
+                </td>
+                <td class="px-6 py-4">
+                  <a href="http://localhost:8080/admin/studentDetails.php?id=<?php echo $list["id"] ?>&company_id=<?php echo $list["company_id"]; ?>&status_id=<?php echo $list["status_id"] ?>" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                    <?php echo $list["id"] ?>
+                  </a>
+                </td>
+              </tr>
+              <?php }?>
+            </tbody>
+          </table>
+        </div>
+      </section>
       <section class="text-center">
         <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 " onclick="goback()">企業一覧へ</button>
       </section>
