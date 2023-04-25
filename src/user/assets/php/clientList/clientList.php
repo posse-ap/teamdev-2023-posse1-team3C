0ca168469237418b078d094f272e2479f66f84d0
uenoarisa <arisa-choco0326@docomo.ne.jp>
2023-04-25 20:40:24 +0900

    修正完了

--- src/user/assets/php/clientList/clientList.php
+++ src/user/assets/php/clientList/clientList.php
@@ -2,7 +2,8 @@
 
 include_once('../dbconnect.php');
 
-$sql_companies = "SELECT * FROM `Companies` as c LEFT OUTER JOIN CompaniesDetails as cd ON cd.detail_id = c.id  LEFT OUTER JOIN `Ratings` as ra ON ra.Rating_id = c.id";
+//企業情報＋星の数持ってくる
+$sql_companies = "SELECT c.id,c.URL,cd.photo,ra.people,ra.support,ra.achivement,ra.speed,ra.amount FROM `Companies` as c LEFT OUTER JOIN CompaniesDetails as cd ON cd.detail_id = c.id  LEFT OUTER JOIN `Ratings` as ra ON ra.Rating_id = c.id";
 $companies = $dbh->query($sql_companies)->fetchAll(PDO::FETCH_ASSOC);
 
 foreach ($companies as $company) {
@@ -16,8 +17,10 @@ foreach ($companies as $company) {
   print_r($company['achievement']); //星
   print_r($company['speed']); //星
   print_r($company['amount']); //星
+  //コピペで使ってください
   //[rating_id] => 1 [people] => 3 [support] => 4 [achievement] => 2 [speed] => 4 [amount] => 5 
 
+  //推しポイント取得
   $sql_points = $dbh->prepare("SELECT * FROM `GoodPoints` WHERE company_id = :company_id");
   $sql_points->bindValue(':company_id', $company_id);
   $sql_points->execute();
