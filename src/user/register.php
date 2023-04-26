4c4473caa7216419b74e8818fd3af94138eb30e7
dukken06 <kenxavi.8@gmail.com>
2023-04-26 11:08:38 +0900

    register.phpの修正

--- src/user/register.php
+++ src/user/register.php
@@ -22,28 +22,5 @@
   <main>
 
   </main>
-  <!-- フッター -->
-  <footer>
-    <div class="footer-boozer-logo">
-      <img src="./assets/img/boozer_logo_white.png" alt="boozer">
-    </div>
-    <ul class="footer-link-list">
-      <li>
-        お問い合わせ
-      </li>
-      <li>
-        運営会社
-      </li>
-      <li>
-        利用規約
-      </li>
-      <li>
-        プライバシーポリシー
-      </li>
-    </ul>
-    <div class="footer-craft">
-      © 2023 CRAFT
-    </div>
-  </footer>
 </body>
 </html>
