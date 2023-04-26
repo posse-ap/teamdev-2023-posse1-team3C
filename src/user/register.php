3551bf250dfcfbdc22c4e76f22307df81d7b33a6
dukken06 <kenxavi.8@gmail.com>
2023-04-27 00:24:41 +0900

    性別まで

--- src/user/register.php
+++ src/user/register.php
@@ -41,11 +41,12 @@
         <label for="sex" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">性別</label>
         <select id="sex" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
           <option selected></option>
-          <option value="US">男性</option>
-          <option value="CA">女性</option>
-          <option value="FR">未回答</option>
+          <option value="man">男性</option>
+          <option value="woman">女性</option>
+          <option value="no">未回答</option>
         </select>
       </div>
+      
       <div class="mb-6">
         <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email address</label>
         <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe@company.com" required>
