aba6b90adc10b0238ab51bb3396d149167778ef7
kazuki1023 <k.keio12569@gmail.com>
2023-04-27 12:24:33 +0900

    ファイル、星評価見た目完成

--- src/admin/register.php
+++ src/admin/register.php
@@ -63,7 +63,74 @@ include(dirname(__FILE__) . '/components/head.php');
           </div>
         </section>
         <!-- 掲載情報登録 -->
-        <section></section>
+        <section>
+          <h3>掲載情報登録</h3>
+          <div>
+            <h4>企業宣材写真</h4>
+            <div>
+              <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload file</label>
+              <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file">
+            </div>
+          </div>
+          <div>
+            <h4>星評価(0~5で評価)</h4>
+            <div class="flex gap-5 justify-end">
+              <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">求人数</label>
+              <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-4/5 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option selected></option>
+                <option value="5">5</option>
+                <option value="4">4</option>
+                <option value="3">3</option>
+                <option value="2">2</option>
+                <option value="1">1</option>
+              </select>
+            </div>
+            <div class="flex gap-5 justify-end">
+              <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">サポート力</label>
+              <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-4/5 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option selected></option>
+                <option value="5">5</option>
+                <option value="4">4</option>
+                <option value="3">3</option>
+                <option value="2">2</option>
+                <option value="1">1</option>
+              </select>
+            </div>
+            <div class="flex gap-5 justify-end">
+              <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">内定獲得実績</label>
+              <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-4/5 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option selected></option>
+                <option value="5">5</option>
+                <option value="4">4</option>
+                <option value="3">3</option>
+                <option value="2">2</option>
+                <option value="1">1</option>
+              </select>
+            </div>
+            <div class="flex gap-5 justify-end">
+              <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">内定速度</label>
+              <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-4/5 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option selected></option>
+                <option value="5">5</option>
+                <option value="4">4</option>
+                <option value="3">3</option>
+                <option value="2">2</option>
+                <option value="1">1</option>
+              </select>
+            </div>
+            <div class="flex gap-5 justify-end">
+              <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">業界の多さ</label>
+              <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-4/5 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option selected></option>
+                <option value="5">5</option>
+                <option value="4">4</option>
+                <option value="3">3</option>
+                <option value="2">2</option>
+                <option value="1">1</option>
+              </select>
+            </div>
+          </div>
+        </section>
         <!-- 登録ボタン -->
         <section class="mx-4">
           <div class="flex items-start mb-6">
