680fb2128985f87e39dff48a9295495c43697dfe
kazuki1023 <k.keio12569@gmail.com>
2023-05-03 09:28:56 +0900

    推しポイントのフロント完成

--- src/admin/register.php
+++ src/admin/register.php
@@ -90,7 +90,7 @@ include(dirname(__FILE__) . '/components/head.php');
           <h3>掲載情報登録</h3>
           <!-- ヒーロー画像 -->
           <div>
-            <h4>企業宣材写真</h4>
+            <h3>企業宣材写真</h3>
             <div>
               <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">写真を選択してください</label>
               <input name="heroImage" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file">
@@ -98,7 +98,7 @@ include(dirname(__FILE__) . '/components/head.php');
           </div>
           <!-- 星評価 -->
           <div>
-            <h4>星評価(0~5で評価)</h4>
+            <h3>星評価(0~5で評価)</h3>
             <div class="flex gap-5 justify-end">
               <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">求人数</label>
               <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-4/5 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="peopleRate">
@@ -318,7 +318,7 @@ include(dirname(__FILE__) . '/components/head.php');
           </div>
           <!-- サクセスストーリー -->
           <div>
-            <h4>サクセスストーリ</h4>
+            <h3>サクセスストーリ</h3>
             <div class="mb-8">
               <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="successFirst">写真を選択してください</label>
               <input name="successFirst" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="successFirst" type="file">
@@ -360,6 +360,22 @@ include(dirname(__FILE__) . '/components/head.php');
               <input type="text" id="successThirdTitle" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="successThirdTitle">
               <label for="successThirdDescription" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">詳細説明</label>
               <textarea id="successThirdDescription" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..." name="successThirdDescription"></textarea>
+            </div>
+          </div>
+          <!-- 推しポイント -->
+          <div>
+            <h3>推しポイント</h3>
+            <div class="mb-6">
+              <label for="goodPointFirst" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">推しポイント1</label>
+              <input type="text" id="goodPointFirst" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="goodPointFirst">
+            </div>
+            <div class="mb-6">
+              <label for="goodPointSecond" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">推しポイント2</label>
+              <input type="text" id="goodPointSecond" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="goodPointSecond">
+            </div>
+            <div class="mb-6">
+              <label for="goodPointThird" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">推しポイント3</label>
+              <input type="text" id="goodPointThird" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="goodPointThird">
             </div>
           </div>
         </section>
