03861c9d4271a4f33db6e9048078d93d49f97ba4
kazuki1023 <k.keio12569@gmail.com>
2023-05-03 18:46:53 +0900

    checkつけたら送信ボタンを押せるようにする

--- src/admin/register.php
+++ src/admin/register.php
@@ -384,10 +384,10 @@ include(dirname(__FILE__) . '/components/head.php');
           <div class="flex items-start mb-6">
             <div class="flex items-center h-5">
               <input id="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required>
+              <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">誤りが無いことを確認しました。</label>
+              <button type="submit" name="registerButton" class= "inactive text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 registerButton inactive">Submit</button>
             </div>
-            <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">誤りが無いことを確認しました。</label>
           </div>
-          <button type="submit" name="registerButton" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
         </section>
       </form>
     </main>
