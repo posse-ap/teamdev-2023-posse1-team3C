ed270bcd9e702a2232f4016723c34fc79df9485d
dukken06 <kenxavi.8@gmail.com>
2023-05-05 01:49:56 +0900

    ポップ1ページ目のjs完了

--- src/user/assets/scripts/index.js
+++ src/user/assets/scripts/index.js
@@ -1,8 +1,22 @@
 // モーダルを表示
+const pop1 = document.querySelector('.pop-1');
+const popArea1 = document.querySelector('.pop-area-1');
+// トップページを開いたら、ポップ1ぺージ目を表示
+window.addEventListener('load', () => {
+  popArea1.style.opacity = '1';
+});
 // トップページを開いたとき(ポップ1ページ目)→(次へ)→ポップ2ページ目
-
+const pop1NextBtn = document.querySelector('.pop-1-next-btn button');
+const pop2 = document.querySelector('.pop-2');
+pop1NextBtn.addEventListener('click', () => {
+  pop1.classList.remove('active');
+  pop2.classList.add('active');
+});
 // トップページを開いたとき(ポップ1ページ目)→(スキップする)→ポップを閉じる
-
+const pop1SkipBtn = document.querySelector('.pop-1-skip-btn button');
+pop1SkipBtn.addEventListener('click', () => {
+  pop1.classList.remove('active');
+});
 // 利用者の声のスライドショー
 let mySwiper = new Swiper ('.swiper', {
   loop: true,
