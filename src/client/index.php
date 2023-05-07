04a3c2a79441e3416441e4893509d5d372624ba6
uenoarisa <arisa-choco0326@docomo.ne.jp>
2023-05-07 10:50:55 +0900

    client修正完了

--- src/client/index.php
+++ src/client/index.php
@@ -1,3 +1,8 @@
+<?php session_start();
+  if(!isset($_SESSION['unique_id'])){
+    header("location: auth/signin.php");
+  }
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -9,26 +14,42 @@
 <body>
   <?php include_once('components/link.php') ?>
   <?php include_once('assets/php/index.php') ?>
-  <header class="sticky top-20 z-50">
-    <h1 class="my-3 mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-center" >HOME</h1>
+  <header class="sticky top-10 z-50">
+    <h1 class="my-1 mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-center" >HOME</h1>
   </header>
 
 <section class="text-gray-600 body-font relative">
   
-  <div class="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap w-3/4">
+  <div class="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap w-4/5">
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg p-8">
+      <div class="flex">
         <h3 class="text-xl">登録学生情報</h3>
-        <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">月 : </label>
-        <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-          <option selected>月を選択してください</option>
-          <option value="US">United States</option>
-          <option value="CA">Canada</option>
-          <option value="FR">France</option>
-          <option value="DE">Germany</option>
+        
+        <form action="">
+        <input type="hidden" name="company_id" value="<?php echo $_SESSION["unique_id"] ?>" id="input">
+        <button type="submit" class="ml-20 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg" id="download-btn">CSVダウンロード</button>
+        </form>
+      </div>
+      <form method="post" action="#">
+  <!-- input fields and select element here -->
+        <label for="month" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">月 : </label>
+        <select id="month" name="month" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+          <option value="20" selected>月を選択してください</option>
+          <option value="2">全ての月を選択する</option>
+          <option value="2022-10">2022/10</option>
+          <option value="2022-11">2022/11</option>
+          <option value="2022-12">2022/12</option>
+          <option value="2023-01">2023/1</option>
+          <option value="2023-02">2023/2</option>
+          <option value="2023-03">2023/3</option>
+          <option value="2023-04">2023/4</option>
+          <option value="2023-05">2023/5</option>
+          <option value="2023-06">2023/6</option>
         </select>
-        <button class="my-12  ml-44 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg" id="download-btn">CSVダウンロード</button>
+        <input type="submit" value="Submit" id="month-btn">
+        </form>
 
-        <p class="m-3.5">有効学生数 :  <span><?= $valid ?></span>人（無効学生数 : <span><?= $invalid ?>人<span>)</p>
+        <p class="m-3.5">有効学生数 :  <span class="text-4xl"><?= $valid ?></span>人（無効学生数 : <span class="text-4xl"> <?= $invalid ?> </span> 人)</p>
         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
@@ -46,7 +67,7 @@
                     </th>
                 </tr>
             </thead>
-            <tbody>
+            <tbody id="tbody">
               <?php foreach ($students as $student)  { ?>
                 <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
