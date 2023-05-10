dcced0386d883c2d8fc7fedc8ea0bfcbb87014f1
Tatataiki <taiki42097147711215@icloud.com>
2023-05-10 23:59:06 +0900

    ヘッダーとフッター入れた

---
+++ src/user/favorite.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CRAFT 企業詳細ページ</title>
+  <link rel="stylesheet" href="./assets/styles/normalize.css">
+  <link rel="stylesheet" href="./assets/styles/common.css">
+  <link rel="stylesheet" href="./assets/styles/favorite.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js" defer></script>
+  <script src="./assets/scripts/favorite/favorite.js" defer></script>
+</head>
+
+<body>
+<?php include_once('components/header.php') ?>
+
+<main>
+  idhifofhi
+</main>
+<!-- フッター -->
+<?php include_once('components/footer.php') ?>
+  <script src="https://kit.fontawesome.com/b4c9445cdc.js" crossorigin="anonymous"></script>
+  <script src="./assets/scripts/clientList/clientList.js"></script>
+</body>
+
+</html>
