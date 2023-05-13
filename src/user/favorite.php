cc17885023823c334997078d4335f0fe4b6cdf12
Tatataiki <taiki42097147711215@icloud.com>
2023-05-14 01:02:33 +0900

    favoriteが復活した

--- src/user/favorite.php
+++ src/user/favorite.php
@@ -18,7 +18,118 @@
 <?php include_once('components/header.php') ?>
 
 <main>
-  idhifofhi
+  <div class="top-favorite">お気に入り一覧</div>
+
+
+  <div class="lets-favorite">現在のあなたのお気に入り一覧はこちら<br><br>チェックをつけて一括登録しよう！</div>
+
+
+
+  <div class="service-box">
+    <div class="container">
+      <label>
+        <input type="checkbox" class="checkbox">
+        <span class="checkbox-fontas"></span>
+      </label>
+      <div class="service-name">サービスA</div>
+      <div class="browsing-history">最終閲覧履歴2023/02/21</div>
+    </div>
+    <div class="button-container">
+      <div class="official-page">公式ページ&nbsp;□</div>
+      <div class="detail-page">詳細ページ&nbsp;></div>
+      <div class="subscribe-favorite">♡&nbsp;お気に入り登録</div>
+    </div>
+  </div>
+
+
+  <div class="service-box">
+    <div class="container">
+      <div class="checkbox">□</div>
+      <div class="service-name">サービスF</div>
+      <div class="browsing-history">最終閲覧履歴2023/02/21</div>
+    </div>
+    <div class="button-container">
+      <div class="official-page">公式ページ&nbsp;□</div>
+      <div class="detail-page">詳細ページ&nbsp;></div>
+      <div class="subscribe-favorite">♡&nbsp;お気に入り登録</div>
+    </div>
+  </div>
+
+
+  <div class="service-box">
+    <div class="container">
+      <div class="checkbox">□</div>
+      <div class="service-name">サービスD</div>
+      <div class="browsing-history">最終閲覧履歴2023/02/21</div>
+    </div>
+    <div class="button-container">
+      <div class="official-page">公式ページ&nbsp;□</div>
+      <div class="detail-page">詳細ページ&nbsp;></div>
+      <div class="subscribe-favorite">♡&nbsp;お気に入り登録</div>
+    </div>
+  </div>
+
+
+  <div class="service-box">
+    <div class="container">
+      <div class="checkbox">□</div>
+      <div class="service-name">サービスC</div>
+      <div class="browsing-history">最終閲覧履歴2023/02/21</div>
+    </div>
+    <div class="button-container">
+      <div class="official-page">公式ページ&nbsp;□</div>
+      <div class="detail-page">詳細ページ&nbsp;></div>
+      <div class="subscribe-favorite">♡&nbsp;お気に入り登録</div>
+    </div>
+  </div>
+
+
+  <div class="service-box">
+    <div class="container">
+      <div class="checkbox">□</div>
+      <div class="service-name">サービスE</div>
+      <div class="browsing-history">最終閲覧履歴2023/02/21</div>
+    </div>
+    <div class="button-container">
+      <div class="official-page">公式ページ&nbsp;□</div>
+      <div class="detail-page">詳細ページ&nbsp;></div>
+      <div class="subscribe-favorite">♡&nbsp;お気に入り登録</div>
+    </div>
+  </div>
+
+
+  <div class="service-box">
+    <div class="container">
+      <div class="checkbox">□</div>
+      <i class="fa-solid fa-circle-check"></i>
+      <div class="service-name">サービスB</div>
+      <div class="browsing-history">最終閲覧履歴2023/02/21</div>
+    </div>
+    <div class="button-container">
+      <div class="official-page">公式ページ&nbsp;□</div>
+      <div class="detail-page">詳細ページ&nbsp;></div>
+      <div class="subscribe-favorite">♡&nbsp;お気に入り登録</div>
+    </div>
+  </div>
+
+
+
+  <ul class="pagination">
+<li><a class="prev" href="#">前へ</a></li>
+<li><span aria-current="page">1</span></li>
+<li><a href="#">2</a></li>
+<li><a href="#">3</a></li>
+<li><a href="#">4</a></li>
+<li><a href="#">5</a></li>
+<li><a class="next" href="#">次へ</a></li>
+</ul>
+
+
+  <div></div>
+
+  <div class="lets-favorite">＼気になったエージェントと話してみよう／</div>
+  <div class="subscribe">一括登録する&nbsp;▶︎</div>
+
 </main>
 <!-- フッター -->
 <?php include_once('components/footer.php') ?>
