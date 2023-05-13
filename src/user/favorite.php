a61b9909c399b45b9eab6a4e4cfbb426c95db1d1
Tatataiki <taiki42097147711215@icloud.com>
2023-05-14 03:08:57 +0900

    レスポンシブ以外終了

--- src/user/favorite.php
+++ src/user/favorite.php
@@ -18,7 +18,7 @@
 <?php include_once('components/header.php') ?>
 
 <main>
-  <div class="top-favorite">お気に入り一覧</div>
+  <h2 class="top-favorite">お気に入り一覧</h2>
 
 
   <div class="lets-favorite">現在のあなたのお気に入り一覧はこちら<br><br>チェックをつけて一括登録しよう！</div>
@@ -31,104 +31,31 @@
         <input type="checkbox" class="checkbox">
         <span class="checkbox-fontas"></span>
       </label>
-      <div class="service-name">サービスA</div>
-      <div class="browsing-history">最終閲覧履歴2023/02/21</div>
+      <div class="s-container">
+        <div class="service-name">サービスA</div>
+        <div class="browsing-history">最終閲覧履歴2023/02/21</div>
+      </div>
     </div>
     <div class="button-container">
-      <div class="official-page">公式ページ&nbsp;□</div>
-      <div class="detail-page">詳細ページ&nbsp;></div>
-      <div class="subscribe-favorite">♡&nbsp;お気に入り登録</div>
-    </div>
-  </div>
-
-
-  <div class="service-box">
-    <div class="container">
-      <div class="checkbox">□</div>
-      <div class="service-name">サービスF</div>
-      <div class="browsing-history">最終閲覧履歴2023/02/21</div>
-    </div>
-    <div class="button-container">
-      <div class="official-page">公式ページ&nbsp;□</div>
-      <div class="detail-page">詳細ページ&nbsp;></div>
-      <div class="subscribe-favorite">♡&nbsp;お気に入り登録</div>
-    </div>
-  </div>
-
-
-  <div class="service-box">
-    <div class="container">
-      <div class="checkbox">□</div>
-      <div class="service-name">サービスD</div>
-      <div class="browsing-history">最終閲覧履歴2023/02/21</div>
-    </div>
-    <div class="button-container">
-      <div class="official-page">公式ページ&nbsp;□</div>
-      <div class="detail-page">詳細ページ&nbsp;></div>
-      <div class="subscribe-favorite">♡&nbsp;お気に入り登録</div>
-    </div>
-  </div>
-
-
-  <div class="service-box">
-    <div class="container">
-      <div class="checkbox">□</div>
-      <div class="service-name">サービスC</div>
-      <div class="browsing-history">最終閲覧履歴2023/02/21</div>
-    </div>
-    <div class="button-container">
-      <div class="official-page">公式ページ&nbsp;□</div>
-      <div class="detail-page">詳細ページ&nbsp;></div>
-      <div class="subscribe-favorite">♡&nbsp;お気に入り登録</div>
-    </div>
-  </div>
-
-
-  <div class="service-box">
-    <div class="container">
-      <div class="checkbox">□</div>
-      <div class="service-name">サービスE</div>
-      <div class="browsing-history">最終閲覧履歴2023/02/21</div>
-    </div>
-    <div class="button-container">
-      <div class="official-page">公式ページ&nbsp;□</div>
-      <div class="detail-page">詳細ページ&nbsp;></div>
-      <div class="subscribe-favorite">♡&nbsp;お気に入り登録</div>
-    </div>
-  </div>
-
-
-  <div class="service-box">
-    <div class="container">
-      <div class="checkbox">□</div>
-      <i class="fa-solid fa-circle-check"></i>
-      <div class="service-name">サービスB</div>
-      <div class="browsing-history">最終閲覧履歴2023/02/21</div>
-    </div>
-    <div class="button-container">
-      <div class="official-page">公式ページ&nbsp;□</div>
-      <div class="detail-page">詳細ページ&nbsp;></div>
-      <div class="subscribe-favorite">♡&nbsp;お気に入り登録</div>
+      <a href="" target="_blank">
+        <div class="official-page">公式ページ<i class="fa-solid     fa-arrow-up-right-from-square"></i></div>
+      </a>
+      <a href="">
+        <div class="detail-page">詳細ページ<i class="fa-solid     fa-chevron-right"></i></div>
+      </a>
+      <div class="subscribe-favorite"><i class="fa-solid fa-heart" style="color: #ff6b97;"></i>お気に入り登録</div>
     </div>
   </div>
 
 
 
-  <ul class="pagination">
-<li><a class="prev" href="#">前へ</a></li>
-<li><span aria-current="page">1</span></li>
-<li><a href="#">2</a></li>
-<li><a href="#">3</a></li>
-<li><a href="#">4</a></li>
-<li><a href="#">5</a></li>
-<li><a class="next" href="#">次へ</a></li>
-</ul>
+
 
 
   <div></div>
 
   <div class="lets-favorite">＼気になったエージェントと話してみよう／</div>
-  <div class="subscribe">一括登録する&nbsp;▶︎</div>
+  <div class="subscribe">一括登録する<i class="fa-solid fa-play"></i></div>
 
 </main>
 <!-- フッター -->
