5e76fbcf1dc48fa8c4a71a33bc6d4647d4e711d5
dukken06 <kenxavi.8@gmail.com>
2023-05-17 00:20:54 +0900

    admin修正終わり

--- src/admin/components/sidebar.php
+++ src/admin/components/sidebar.php
@@ -29,14 +29,6 @@
               <span class="flex-1 ml-3 whitespace-nowrap">掲載企業一覧</span>
             </a>
           </li>
-          <li>
-            <a href="./invalid.php" class="flex items-center p-4 text-white rounded-lg dark:text-white dark:text-white hover:bg-blue-900 dark:hover:bg-blue-700 menu-item">
-              <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path>
-              </svg>
-              <span class="flex-1 ml-3 whitespace-nowrap">無効申請一覧</span>
-            </a>
-          </li>
         </ul>
       </div>
     </aside>
