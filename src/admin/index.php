67ee99d26e559eed2a9948aba155d25fc447cf75
dukken06 <kenxavi.8@gmail.com>
2023-05-16 21:59:48 +0900

    adminトップページ修正

--- src/admin/index.php
+++ src/admin/index.php
@@ -17,9 +17,10 @@ include(dirname(__FILE__) . '/components/head.php');
     <?php include(dirname(__FILE__) . '/components/sidebar.php'); ?>
     <main class="pt-20">
       <h2 class="w-4/5 text-center page-title">Dashboard</h2>
-      <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <div class="relative overflow-x-auto student-info-container">
+        <h3>学生情報一覧</h3>
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 student-info-table">
+          <thead class="text-xs text-gray-700 uppercase student-info-table-title">
             <tr class="text-center">
               <th scope="col" class="px-6 py-3">
                 登録学生氏名
@@ -42,7 +43,7 @@ include(dirname(__FILE__) . '/components/head.php');
             </tr>
           </thead>
           <tbody>
-            <?php foreach ($CompaniesStudentsLink as $list) { ?>　
+            <?php foreach ($CompaniesStudentsLink as $list) { ?>
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 text-center">
               <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                 <?php echo $list["Name"] ?>
@@ -64,7 +65,7 @@ include(dirname(__FILE__) . '/components/head.php');
                   <input type="hidden" name="student_id" value="<?php echo $list["student_id"] ?>">
                   <input type="hidden" name="company_id" value="<?php echo $list["company_id"]; ?>">
                   <input type="hidden" name="status_id" value="<?php echo $list["status_id"] ?>">
-                  <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 " name="studentDetailsButton">学生詳細へ
+                  <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 student-details-btn" name="studentDetailsButton">学生詳細へ
                   </button>
                 </form>
               </td>
