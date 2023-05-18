37508cb89d278b648cf71f2b454610c294ceaba7
dukken06 <kenxavi.8@gmail.com>
2023-05-18 14:51:11 +0900

    履歴レスポンシブ完了

--- src/user/record.php
+++ src/user/record.php
@@ -30,33 +30,7 @@
   <div class="lets-favorite">あなたがチェックした企業はこちら</div>
 
   <div class="service-wrapper" id='service-wrapper'>
-  <div class="service-box" id="service_box">
-  <div class="container">
-  <label>
-    <input type="checkbox" class="checkbox">
-    <span class="checkbox-fontas"></span>
-  </label>
-  <div class="s-container">
-    <div class="service-name">$companyName</div>
-    <div class="browsing-history">最終閲覧履歴<br>$companytime</div>
-  </div>
-</div>
-<div class="button-container">
-  <a href="$companyURL" target="_blank">
-    <div class="official-page">公式サイト<i class="fa-solid     fa-arrow-up-right-from-square"></i></div>
-  </a>
-  <a href="clientDetails.php?id=$companyID">
-    <div class="detail-page">詳細ページ<i class="fa-solid     fa-chevron-right"></i></div>
-  </a>
-  <button type="button" class="favorite-btn" value="" id="favoriteButton" data-name="<?= $company_details["company"]?>" data-url="<?= $company_details["URL"]?>" data-id="<?= $company_details["id"]?>" onclick="addToFavorites()">
-    <span class="favorite-btn-text">
-      お気に入りに追加
-    </span>
-  </button>
-</div>
-</div>
-</div>
-</div>
+  
   </div>
 
   <div class="lets-favorite">＼気になったエージェントと話してみよう／</div>
