0c0162e802e406d20e190c1f1a56dc9e6c86c704
kazuki1023 <k.keio12569@gmail.com>
2023-05-20 16:50:32 +0900

    履歴のお気に入りボタンからお気に入りできるようになる

--- src/user/assets/scripts/record/record.js
+++ src/user/assets/scripts/record/record.js
@@ -1,9 +1,10 @@
+// 履歴の情報をローカルストレージから取得
 const favorites = JSON.parse(localStorage.getItem("histories")) || [];
-
+localStorage.clear();
 // お気に入り企業の情報をHTMLに書き換え
 const service_box = document.getElementById("service-wrapper");
 
-let str = '';
+let str = "";
 
 favorites.forEach((favorite) => {
   const companyName = favorite.name;
@@ -31,7 +32,7 @@ favorites.forEach((favorite) => {
   <a href="clientDetails.php?id=${companyID}">
     <div class="detail-page">詳細ページ<i class="fa-solid     fa-chevron-right"></i></div>
   </a>
-  <button type="button" class="favorite-btn" value="" id="favoriteButton" data-name="${companyName}" data-url="${companyURL}" data-id="${companyID}" onclick="addToFavorites()">
+  <button type="button" class="favorite-btn" value="" id="favoriteButton" data-name="${companyName}" data-url="${companyURL}" data-id="${companyID}" onclick="addToFavorites(this)">
     <span class="favorite-btn-text">
       お気に入りに追加
     </span>
@@ -45,54 +46,78 @@ favorites.forEach((favorite) => {
 service_box.innerHTML = str;
 
 // お気に入りボタンを押したら色が変わる、テキストが変更される
-let favorite_btns = document.querySelectorAll('.favorite-btn');
-let favoriteTexts = document.querySelectorAll('.favorite-btn-text');
+let favorite_btns = document.querySelectorAll(".favorite-btn");
+let favoriteTexts = document.querySelectorAll(".favorite-btn-text");
 
 favorite_btns.forEach((favorite_btn, index) => {
-  favorite_btn.addEventListener('click', function() {
-    if (favorite_btn.classList.contains('active')) {
-      favorite_btn.classList.remove('active');
-      favoriteTexts[index].textContent = 'お気に入りに追加';
+  favorite_btn.addEventListener("click", function () {
+    if (favorite_btn.classList.contains("active")) {
+      favorite_btn.classList.remove("active");
+      favoriteTexts[index].textContent = "お気に入りに追加";
       favorite_btn.value = 0;
     } else {
-      favorite_btn.classList.add('active');
-      favoriteTexts[index].textContent = 'お気に入り済み';
+      favorite_btn.classList.add("active");
+      favoriteTexts[index].textContent = "お気に入り済み";
       favorite_btn.value = 1;
     }
-    addToFavorites(favorite_btn);
+    // addToFavorites(favorite_btn);
   });
 });
 
-
-function addToFavorites() {
-  let favorite_btn = document.querySelector('.favorite-btn');
-  let companyID = favorite_btn.getAttribute('data-id');
+function addToFavorites(button) {
+  // 企業の情報（仮のデータ）
+  let companyName = button.getAttribute("data-name");
+  let companyURL = button.getAttribute("data-url");
+  let companyID = button.getAttribute("data-id");
   let currentTime = new Date().toLocaleString();
-  // ローカルストレージからお気に入り情報を取得
-  const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+  // localStorageお気に入り情報があるかどうかチェック
+  if (localStorage.favorites !== undefined) {
+    // let favorite_btn = document.querySelector('.favorite-btn');
+    // console.log(favorite_btn);
+    // let companyID = favorite_btn.getAttribute('data-id');
+    console.log(button);
 
-  if (favorite_btn.value == 0) {
-    // 既に同じIDの企業がローカルストレージ内に存在するかチェック
-    const existingIndex = favorites.findIndex(item => item.id === companyID);
+    console.log(companyID);
 
-    if (existingIndex !== -1) {
-      // 同じIDの企業が既に登録されている場合は削除
+    // ローカルストレージからお気に入り情報を取得
+    const favorites = JSON.parse(localStorage.favorites);
+
+    if (button.value == 0) {
+      // 既に同じIDの企業がローカルストレージ内に存在するかチェック
+      const existingIndex = favorites.findIndex(
+        (item) => item.id === companyID
+      );
+
+      if (existingIndex !== -1) {
+        // 同じIDの企業が既に登録されている場合は削除
+        favorites.splice(existingIndex, 1);
+      }
+      // お気に入り情報を追加
+      favorites.push({
+        name: companyName,
+        url: companyURL,
+        id: companyID,
+        time: currentTime,
+      });
+
+      // ローカルストレージに保存
+      localStorage.setItem("favorites", JSON.stringify(favorites));
+    } else {
+      const existingIndex = favorites.findIndex(
+        (item) => item.id === companyID
+      );
       favorites.splice(existingIndex, 1);
+      localStorage.setItem("favorites", JSON.stringify(favorites));
     }
-
-    // 企業の情報（仮のデータ）
-    let companyName = favorite_btn.getAttribute('data-name');
-    let companyURL = favorite_btn.getAttribute('data-url');
-
-    // お気に入り情報を追加
-    favorites.push({ name: companyName, url: companyURL, id: companyID, time: currentTime });
-
-    // ローカルストレージに保存
-    localStorage.setItem("favorites", JSON.stringify(favorites));
   } else {
-    const existingIndex = favorites.findIndex(item => item.id === companyID);
-    favorites.splice(existingIndex, 1);
-    localStorage.setItem("favorites", JSON.stringify(favorites));
+    // lcocalStorageにお気に入り情報がない場合
+    const favorites = [{
+      name: companyName,
+      url: companyURL,
+      id: companyID,
+      time: currentTime,
+    }];
+    localStorage.setItem("favorites", JSON.stringify((favorites)));
   }
 }
 
