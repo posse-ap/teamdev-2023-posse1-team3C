7a8c7c03a74db22d1f3d8cef047a9f8b87ff0247
kazuki1023 <k.keio12569@gmail.com>
2023-05-23 11:02:01 +0900

    企業登録エラーは出ない。あとテスト

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -252,7 +252,7 @@ CREATE TABLE Ratings (
   amount INT
 );
 
-insert into Ratings (people,support, achievement, speed, amount) values (3, 4, 2, 4, 5), (4,3,4,5,2), (5,4,3,4,3), (3,4,4,5,4),(3, 4, 4, 5, 4),(4, 3, 4, 5, 2),(5, 3, 3, 2, 5),(1, 5, 4, 3, 4),(2, 4, 5, 4, 3),(4, 2, 5, 3, 4),(3, 4, 2, 5, 3),(5, 3, 4, 4, 2),(3, 5, 4, 2, 4),(2, 4, 3, 4, 5);
+insert into Ratings (people,support, achievement, speed, amount) values (3, 4, 2, 4, 5), (4,3,4,5,2), (5,4,3,4,3), (3,4,4,5,4),(3, 4, 4, 5, 4),(4, 3, 4, 5, 2),(5, 3, 3, 2, 5),(1, 5, 4, 3, 4),(2, 4, 5, 4, 3),(4, 2, 5, 3, 4);
 
 DROP TABLE IF EXISTS Students;
 
