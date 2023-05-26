dcc975c68e176f33592def1c8f3afea4ee22890a
kazuki1023 <k.keio12569@gmail.com>
2023-05-26 15:26:14 +0900

    掲載会前企業を一覧に追加

--- src/admin/clientList.php
+++ src/admin/clientList.php
@@ -101,6 +101,48 @@ include(dirname(__FILE__) . '/components/head.php');
           </tbody>
         </table>
       </div>
+      <!-- 掲載期間がまだ始まってない企業表 -->
+      <div class="relative overflow-x-auto mb-5">
+        <h3 class="w-4/5 text-center page-title">掲載開始前企業</h3>
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 client-info-table">
+          <thead class="text-xs text-gray-700 uppercase dark:text-gray-400 client-info-table-title">
+            <tr>
+              <th scope="col" class="px-6 py-3">
+                企業名
+              </th>
+              <th scope="col" class="px-6 py-3">
+                掲載期間開始日
+              </th>
+              <th scope="col" class="px-6 py-3">
+                掲載期間終了日
+              </th>
+              <th scope="col" class="px-6 py-3">
+                詳細ボタン
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php foreach ($companiesNotStart as $company) { ?>
+              <tr class="border-b border-gray-200 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-white dark:text-white dark:bg-gray-800">
+                  <?php echo $company["Company"]; ?>
+                </th>
+                <td class="px-6 py-4 bg-white">
+                  <?php echo $company["started_at"]; ?>
+                </td>
+                <td class="px-6 py-4 bg-white">
+                  <?php echo $company["finished_at"]; ?>
+                </td>
+                <td class="px-6 py-4 bg-white">
+                  <a href="clientDetails.php?id=<?php echo $company["id"] ?>" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                    詳細
+                  </a>
+                </td>
+              </tr>
+            <?php } ?>
+          </tbody>
+        </table>
+      </div>
     </main>
   </div>
 </body>
