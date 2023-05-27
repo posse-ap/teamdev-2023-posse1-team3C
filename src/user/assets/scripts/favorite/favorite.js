76e4df8ddbbf9299490de14f12f0d34c77916650
kazuki1023 <k.keio12569@gmail.com>
2023-05-27 22:30:36 +0900

    メッセージを改行した

--- src/user/assets/scripts/favorite/favorite.js
+++ src/user/assets/scripts/favorite/favorite.js
@@ -51,7 +51,7 @@ if (favorites.length === 0) {
   <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
   <span class="sr-only">Info</span>
   <div>
-    <span class="font-medium">お気に入り登録されている企業がありません。</span> お気に入りしたい企業を検索して登録してみよう！
+    <span class="font-medium">お気に入り登録されている企業がありません。</span><br>お気に入りしたい企業を検索して登録してみよう！
   </div>
 </div>
   `;
