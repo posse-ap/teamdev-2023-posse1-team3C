f29374aa458f37dc9dfa42babb53789df3b32f08
dukken06 <kenxavi.8@gmail.com>
2023-05-28 03:01:58 +0900

    tailwind.csswo

--- src/user/assets/scripts/record/record.js
+++ src/user/assets/scripts/record/record.js
@@ -1,6 +1,6 @@
 // 履歴の情報をローカルストレージから取得
 const histories = JSON.parse(localStorage.getItem("histories")) || [];
-// localStorage.clear();
+localStorage.clear();
 // お気に入り企業の情報をHTMLに書き換え
 const service_box = document.getElementById("service-wrapper");
 
@@ -49,10 +49,9 @@ const historiesAlert = document.getElementById("service-alert");
 let historiesAlertStr = "";
 if (histories.length === 0) {
   historiesAlertStr += `
-<div class="flex p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800" role="alert">
-  <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
-  <span class="sr-only">Info</span>
+<div class="alert-area" role="alert">
   <div>
+    <i class="fa-solid fa-circle-info"></i>
     <span class="font-medium">チェックした企業が存在しません。</span><br>一覧から気になる企業をチェックしてみよう！
   </div>
 </div>
