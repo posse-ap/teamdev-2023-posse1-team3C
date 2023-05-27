57531f853ad99c70f29a830b9455728ba946ad4b
kazuki1023 <k.keio12569@gmail.com>
2023-05-27 23:04:14 +0900

    tailwindを使うと、cssバグるので、cssで書き換えてほしい

--- src/user/record.php
+++ src/user/record.php
@@ -8,7 +8,7 @@
   <title>CRAFT 企業詳細ページ</title>
   <link rel="stylesheet" href="./assets/styles/normalize.css">
   <link rel="stylesheet" href="./assets/styles/common.css">
-  <link rel="stylesheet" href="../../dist/output.css">
+  <!-- <link rel="stylesheet" href="../../dist/output.css"> -->
   <link rel="stylesheet" href="./assets/styles/record.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <script src="https://code.jquery.com/jquery-3.6.4.min.js" defer></script>
