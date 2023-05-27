02a32593dea43e5ded8ebdc78286c40f7ef71567
kazuki1023 <k.keio12569@gmail.com>
2023-05-28 01:37:53 +0900

    password表示のjs作成

--- src/client/assets/scripts/auth/pass-show-hide.js
+++ src/client/assets/scripts/auth/pass-show-hide.js
@@ -1,16 +1,30 @@
 /*===========================================================*/
 /*パスワードの目隠し*/
 /*===========================================================*/
-const pswrdField = document.querySelector(".form .field input[type='password']"),
-togglebutton = document.querySelectorAll(".form .fa-solid.fa-eye")
+const pswrdField = document.querySelectorAll(".form .field input[type='password']");
 // .formの中の.fieldのinputのtype=がpasswordになってるやつ
 
-togglebutton.onclick = ()=>{
-  if (pswrdField.type == "password"){
+function showPassword(togglebutton) {
+  if (pswrdField.type == "password") {
     pswrdField.type = "text";
     togglebutton.classList.add("active");
-  }else{
+  } else {
     pswrdField.type = "password";
     togglebutton.classList.remove("active");
   }
-}
+};
+
+togglebuttonNow = document.querySelector(".form .fa-solid.fa-eye .toggle-password-now");
+togglebuttonNow.addEventListener("click", function () {
+  showPassword(togglebuttonNow);
+} );
+
+togglebuttonNew = document.querySelector(".form .fa-solid.fa-eye .toggle-password-new");
+togglebuttonNew.addEventListener("click", function () {
+  showPassword(togglebuttonNew);
+} );
+
+togglebuttonConfirm = document.querySelector(".form .fa-solid.fa-eye .toggle-password-confirm");
+togglebuttonConfirm.addEventListener("click", function () {
+  showPassword(togglebuttonConfirm);
+} );
