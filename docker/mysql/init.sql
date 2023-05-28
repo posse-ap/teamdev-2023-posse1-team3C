391d4135227670c821e32e8161dfbba4777bf40b
kazuki1023 <k.keio12569@gmail.com>
2023-05-28 14:50:51 +0900

    星評価変更

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -249,7 +249,7 @@ CREATE TABLE Ratings (
   amount INT
 );
 
-insert into Ratings (people,support, achievement, speed, amount) values (3, 4, 2, 4, 5), (4,3,4,5,2), (5,4,3,4,3), (3,4,4,5,4),(3, 4, 4, 5, 4),(4, 3, 4, 5, 2),(5, 3, 3, 2, 5),(1, 5, 4, 3, 4),(2, 4, 5, 4, 3),(4, 2, 5, 3, 4);
+insert into Ratings (people,support, achievement, speed, amount) values (5, 4, 3, 4, 4), (3,3,4,5,2), (5,4,3,3,3), (4,5,4,3,4),(4, 4, 3, 5, 4),(3, 3, 3, 5, 2),(5, 5, 3, 2, 4),(4, 4, 4, 3, 4),(2, 4, 5, 4, 3),(4, 2, 5, 3, 4);
 
 DROP TABLE IF EXISTS Students;
 
